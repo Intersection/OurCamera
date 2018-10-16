5696290182ff0fe94ed68b1cce7fb81411693665
Alex Bell <alex.bell@intersection.com>
2018-10-15 23:13:52 -0400

    analyze images

---
+++ analyzeimages_test.py
@@ -0,0 +1,17 @@
+import unittest
+from mock import patch
+from saveimages import *
+import copy
+
+class TestAnalyzeImages(unittest.TestCase):
+
+    # Method that gets executed before each test is run in order to set up the test
+    def setUp(self):
+        pass
+
+    # Method that gets executed before each test is run in order to clean up the execution of the previous test.
+    def tearDown(self):
+        pass
+
+    def test_assert(self):
+        assert True
