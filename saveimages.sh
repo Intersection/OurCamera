379df16abf46abc23e82349bb393c9c1589af495
MikhailKazekin <MikhailKazekin@users.noreply.github.com>
2020-06-16 20:53:33 -0400

    Add scripts for running saveimages.py and analyzeimages.py

---
+++ saveimages.sh
@@ -0,0 +1,3 @@
+source envvars-dev
+source venv/ourcamera-dev/bin/activate
+python saveimages.py --access_key $AWS_ACCESS_KEY_ID --secret_key $AWS_SECRET_ACCESS_KEY >> saveimages.log 2>> saveimages.err.log
