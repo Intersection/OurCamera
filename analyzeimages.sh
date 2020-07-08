cb83f1dcf748f11ef3e8c39610cf8d8903b64eb5
MikhailKazekin <MikhailKazekin@users.noreply.github.com>
2020-07-08 12:32:47 -0400

    Remove logging to a file

--- analyzeimages.sh
+++ analyzeimages.sh
@@ -1,5 +1,5 @@
 # set -euxo pipefail
 source envvars-dev
 source venv/ourcamera-dev/bin/activate
-python analyzeimages.py -access_key $AWS_ACCESS_KEY_ID -secret_key $AWS_SECRET_ACCESS_KEY -path_images /tmp/preprocessed -path_labels_map ./data/car_label_map.pbtxt -save_directory /tmp/done >> analyzeimages.log 2>> analyzeimages.err.log
+python analyzeimages.py -access_key $AWS_ACCESS_KEY_ID -secret_key $AWS_SECRET_ACCESS_KEY -path_images /tmp/preprocessed -path_labels_map ./data/car_label_map.pbtxt -save_directory /tmp/done
 
