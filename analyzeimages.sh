cee0b436dba8bda54d790e60da57f82604492e9d
MikhailKazekin <MikhailKazekin@users.noreply.github.com>
2020-07-28 12:10:25 -0400

    Specify envvars file as an argument

--- analyzeimages.sh
+++ analyzeimages.sh
@@ -1,5 +1,4 @@
-# set -euxo pipefail
-source envvars-dev
+source $1
 source venv/ourcamera-dev/bin/activate
 python analyzeimages.py -access_key $AWS_ACCESS_KEY_ID -secret_key $AWS_SECRET_ACCESS_KEY -path_images /tmp/preprocessed -path_labels_map ./data/car_label_map.pbtxt -save_directory /tmp/done
 
